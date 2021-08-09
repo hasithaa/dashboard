070a57a03bcfad6da60f16f8215e1e575d406f0b
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2021-08-09 21:47:01 +0530

    Use Ballerina to fetch data

--- fetchdata.sh
+++
@@ -1,3 +0,0 @@
-curl -H "Authorization: bearer  ${packagePAT}" -X POST -d @pr_fe.json https://api.github.com/graphql > data/PR_FE.json
-curl -H "Authorization: bearer  ${packagePAT}" -X POST -d @pr_ct.json https://api.github.com/graphql > data/PR_CT.json
-curl -H "Authorization: bearer  ${packagePAT}" -X POST -d @pr_rt.json https://api.github.com/graphql > data/PR_RT.json
